48ebbeade8c46a8d0df7904d75c571069b9abe35
the42ndturtle <the42ndturtle@gmail.com>
2019-04-16 22:53:43 -0400

    mic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ Vue.use(VueMaterial.default);
 Vue.config.productionTip = false;
 
 let data = {
-  range: 80,
+  range: 40,
 }
 
 let methods = {
@@ -21,17 +21,34 @@ const vm = new Vue({
   computed: computed
 });
 
+function board(t){
+  for(let x = 1; x < data.range+1; x++){
+    for(let y = 1; y < data.range+1; y++){
+      document.querySelector(`#x${x}y${y}`).style.backgroundColor = `RGBA(${y*t}, ${x*t}, ${x*y*t}, 1)`;
+    }
+  }
+}j
+
 function start(){
   let t = 0;
   let inc = 1;
   setInterval(function(){
     for(let x = 1; x < data.range+1; x++){
       for(let y = 1; y < data.range+1; y++){
-        document.querySelector(`#x${x}y${y}`).style.backgroundColor = `RGBA(${x*t%255}, ${y*t%255}, ${x*y*t%255}, 1)`;
+        document.querySelector(`#x${x}y${y}`).style.backgroundColor = `RGBA(${y*t}, ${x*t}, ${x*y*t}, 1)`;
       }
     }
     t += inc;
     if(t>25) inc = -1;
     else if(t<1) inc = 1;
   },100);
+}
+
+function setup(){
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw(){
+  board((mic.getLevel() * 100));
 }
